e7f8bea3f0dd325531cb796b325fc7db31cfb506
bryce <76966016+brycegallo@users.noreply.github.com>
2023-12-18 19:00:32 -0500

    Add "pwd" functionality allow user to print present working directory

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import subprocess
+import os
 
 def main():
     while True:
-        command = input("$ ")
+        current_directory = os.getcwd()
+        command = input("/" + current_directory.split("/")[-1] + " $ ")
         if command == "quit":
             break
+        if command == "pwd":
+            print(current_directory)
         else:
             print(command)
 
