ac1239bc18c6f7921e8c3551414c23b21d7a4806
bryce <76966016+brycegallo@users.noreply.github.com>
2023-12-18 19:39:41 -0500

    Implement "help" command list some options

--- shellmodule.py
+++ shellmodule.py
@@ -9,5 +9,21 @@ def shell_loop():
             break
         elif command == "pwd":
             print(current_directory)
+        elif command == "ls":
+            ls_process(current_directory)
+        elif command == "helpme":
+            helpme()
         else:
             print(command)
+
+
+def ls_process(current_directory):
+    ls_list = os.listdir(current_directory)
+    ls_output = ""
+    for item in ls_list:
+        ls_output += (item + " ")
+    print(ls_output)
+
+
+def helpme():
+    print("available commands currently include: 'ls', 'pwd', 'quit', 'exit'")
